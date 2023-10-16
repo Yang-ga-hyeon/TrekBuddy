ed7137a3a0a016a8260674e2d44dc0afc17ab2e3
Yang-ga-hyeon <113418750+Yang-ga-hyeon@users.noreply.github.com>
2023-10-17 00:32:37 +0900

    마커찍기까지 완료
    수빈이가 한 부분까지 복붙 완료

---
+++ TrekBuddy/app/src/main/java/com/example/trekbuddy/ui/guide/CourseAdapter.kt
@@ -0,0 +1,79 @@
+package com.example.trekbuddy.ui.guide
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.trekbuddy.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class CourseAdapter(private val coursesList: List<Course>, private val googleMap: GoogleMap?):
+    RecyclerView.Adapter<CourseAdapter.ViewHolder>(){
+    private var selectedItemPosition: Int = RecyclerView.NO_POSITION
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.courselist_item, parent, false)
+        return ViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val course = coursesList[position]
+        holder.courseName.text = course.courseName
+
+        // 이미지를 Glide를 사용하여 로드하고 표시
+        Glide.with(holder.itemView.context)
+            .load(course.imageUrl)
+            .into(holder.courseImage)
+
+        holder.itemView.setOnClickListener {
+            val position = holder.adapterPosition
+            if (selectedItemPosition != position) {
+                // 이전에 선택된 항목이 있으면 선택을 해제하고 새 항목을 선택
+                val previouslySelectedItemPosition = selectedItemPosition
+                selectedItemPosition = position
+                notifyItemChanged(previouslySelectedItemPosition)
+                holder.itemView.setBackgroundColor(Color.YELLOW) // 선택한 항목의 배경색
+
+                // 선택된 항목의 위도와 경도 가져오기
+                val selectedCourse = coursesList[position]
+                val lat = selectedCourse.latitude.toDouble()
+                val lng = selectedCourse.longitude.toDouble()
+                val location = LatLng(lat, lng)
+
+                // 이전에 추가된 마커를 모두 제거
+                googleMap?.clear()
+
+                // 새로운 마커 추가
+                val markerOptions = MarkerOptions()
+                    .position(location)
+                    .title(selectedCourse.courseName)
+                googleMap?.addMarker(markerOptions)
+
+                // 지도를 선택한 위치로 이동
+                googleMap?.moveCamera(CameraUpdateFactory.newLatLng(location))
+            }
+        }
+        // 선택된 항목에 대한 배경색 설정
+        if (position == selectedItemPosition) {
+            holder.itemView.setBackgroundColor(Color.YELLOW) // 선택된 항목의 배경색
+        } else {
+            holder.itemView.setBackgroundColor(Color.WHITE) // 선택되지 않은 항목의 배경색
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return coursesList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val courseName: TextView = itemView.findViewById(R.id.courseNameTextView)
+        val courseImage: ImageView = itemView.findViewById(R.id.courseImageView)
+    }
+}
--- TrekBuddy/app/src/main/java/com/example/trekbuddy/ui/guide/GuideFragment.kt
+++ TrekBuddy/app/src/main/java/com/example/trekbuddy/ui/guide/GuideFragment.kt
@@ -1,20 +1,33 @@
 package com.example.trekbuddy.ui.guide
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.trekbuddy.R
 import com.example.trekbuddy.databinding.FragmentGuideBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class GuideFragment : Fragment() {
 
     private var _binding: FragmentGuideBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CourseAdapter
+    private var googleMap: GoogleMap? = null
+    val db = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +37,76 @@ class GuideFragment : Fragment() {
 
         _binding = FragmentGuideBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        recyclerView = root.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // Firebase에서 데이터를 가져와서 Adapter에 전달
+        val coursesList = ArrayList<Course>() // Firebase에서 가져온 데이터
+
+
+        // Firestore에서 데이터를 가져와 RecyclerView에 표시
+        db.collection("CourseList")
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                val coursesList = ArrayList<Course>()
+                for (document in querySnapshot) {
+                    val courseName = document["name"].toString()
+                    val imageUrl = document["image"].toString()
+                    val latitude = document["latitude"].toString()
+                    val longitude = document["longitude"].toString()
+                    val locationName = document["name"].toString()
+
+                    if (courseName != null && imageUrl != null) {
+                        val course = Course(null, courseName, imageUrl, latitude, longitude, locationName)
+                        coursesList.add(course)
+                    }
+                }
+                adapter = CourseAdapter(coursesList, googleMap)
+                recyclerView.adapter = adapter
+                adapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                // 오류 처리
+            }
 
         return root
+    }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        // 파이어베이스 컬렉션 참조
+        val collectionRef = db.collection("CourseList")
+
+        // 데이터 가져오기
+        collectionRef.get()
+            .addOnSuccessListener { documents ->
+                val locations = ArrayList<LatLng>() // LatLng의 배열로 장소의 위치를 저장
+
+                for (document in documents) {
+                    // Firestore 문서에서 위도와 경도 필드 가져오기
+                    val latitude = document.getDouble("latitude")
+                    val longitude = document.getDouble("longitude")
+                    val locationName = document.getString("name")
+
+                    if (latitude != null && longitude != null && locationName != null) {
+                        val location = LatLng(latitude, longitude)
+
+                        googleMap.addMarker(MarkerOptions().position(location).title(locationName))
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
+                        locations.add(location)
+                    }
+                }
+
+            }
+            .addOnFailureListener { exception ->
+                // 오류 처리
+                Log.e("FirebaseError", "Error getting documents: ${exception.message}")
+            }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
 
     override fun onDestroyView() {
