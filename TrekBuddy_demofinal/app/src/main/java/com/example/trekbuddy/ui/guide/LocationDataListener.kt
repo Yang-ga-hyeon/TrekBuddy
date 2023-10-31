e76a197169a18d307460dcf05b97c77b96404558
Yang-ga-hyeon <113418750+Yang-ga-hyeon@users.noreply.github.com>
2023-10-31 20:21:21 +0900

    Delete TrekBuddy_demofinal directory

--- TrekBuddy_demofinal/app/src/main/java/com/example/trekbuddy/ui/guide/LocationDataListener.kt
+++
@@ -1,6 +0,0 @@
-package com.example.trekbuddy.ui.guide
-import com.google.android.gms.maps.model.LatLng
-
-interface LocationDataListener {
-    fun onLocationDataLoaded(locationArrayList: List<LatLng>, locationNames: List<String>)
-}
--- TrekBuddy_demofinal/app/src/main/java/com/example/trekbuddy/ui/guide/Place.kt
+++
@@ -1,8 +0,0 @@
-package com.example.trekbuddy.ui.guide
-
-import com.google.android.gms.maps.model.LatLng
-
-data class Place(
-    val name: String,
-    val location: LatLng
-)
