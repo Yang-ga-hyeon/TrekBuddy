a901dba0ba5d46073dbe02f3a1a6ccba89c62dbe
Yang-ga-hyeon <113418750+Yang-ga-hyeon@users.noreply.github.com>
2023-10-31 20:20:20 +0900

    Add files via upload

---
+++ TrekBuddy_demofinal/app/src/main/java/com/example/trekbuddy/ui/guide/Course.kt
@@ -0,0 +1,10 @@
+package com.example.trekbuddy.ui.guide
+
+data class Course(
+    val courseId: String?,
+    val courseName: String?,
+    val imageUrl: String?,
+    val latitude: String,
+    val longitude: String,
+    val locationName: String
+)
