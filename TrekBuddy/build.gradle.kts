c53b5d08874d3d4a93122500996f13bb0a01d3a0
Yang-ga-hyeon <113418750+Yang-ga-hyeon@users.noreply.github.com>
2023-11-21 01:58:08 +0900

    완료

--- TrekBuddy/build.gradle.kts
+++ TrekBuddy/build.gradle.kts
@@ -1,10 +1,16 @@
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:1.3.0")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    // Add the dependency for the Google services Gradle plugin 추가됨(가현)
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+
+
 }
